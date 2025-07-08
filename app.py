967e82adde2ad2b2341dfff0e55e6298f930e43c
Hassan-Rana-1 <hassan.inaam.rana@gmail.com>
2025-07-08 15:52:53 -0400

    Created the general user interface for the app

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="chat with multiple PDFs", page_icon=":books:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Asl a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
+
+if __name__ == '__main__':
+    main()
